7f2e28d3e2cf79522a5a224fc4e2d80c687beb66
asherjawad <asher_04@live.comgit clone https://github.com/asherjawad/AngularJS-Learninggit config --global user.email asher_04@live.com>
2014-09-26 19:44:21 +0500

    Knockoutjs Tutorial first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+       
+        <script src="//cdnjs.cloudflare.com/ajax/libs/knockout/3.2.0/knockout-min.js"></script>
+        
+
+        
+    </head>
+    <body>
+       
+    </body>
+
+   
+</html>
